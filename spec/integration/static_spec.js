89536b9b918a33a87e3b89fe6a82616fda340f3b
Richard Chan <rchan817@yahoo.com>
2020-03-24 15:55:28 -0700

    added route and route config for landingpage

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,18 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+  describe("GET /", () => {
+
+    it("should return a status code 200", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    }); // End of "it"
+
+  }); // End of "GET /"" describe
+  
+}); // End of "routes:static" describe
